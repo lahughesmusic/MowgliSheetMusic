74d6f40ac3c4e64589ac2d4d975180877312b0f2
Lea Hughes <lahughesmusic@gmail.com>
2024-06-28 15:36:20 -0500

    file names, adding categories

--- components/Data1.js
+++ components/Data1.js
@@ -2,6 +2,7 @@ const songEntries = [
   {
     id: "1",
     title: "As The Deer",
+    category: "hymn",
     images: [
       require('../assets/hymns/AsTheDeer-0.jpg'),
       require('../assets/hymns/AsTheDeer-1.jpg'),
@@ -12,6 +13,7 @@ const songEntries = [
   {
     id: "2",
     title: "Be Thou My Vision",
+    category: "hymn",
     images: [
       require('../assets/hymns/BeThouMyVision-0.jpg'),
       require('../assets/hymns/BeThouMyVision-1.jpg'),
@@ -21,6 +23,7 @@ const songEntries = [
   {
     id: "3",
     title: "Blessed Assurance",
+    category: "hymn",
     images: [
       require('../assets/hymns/BlessedAssurance-0.jpg'),
       require('../assets/hymns/BlessedAssurance-1.jpg'),
@@ -30,6 +33,7 @@ const songEntries = [
   {
     id: "4",
     title: "His Eye Is On The Sparrow",
+    category: "hymn",
     images: [
       require('../assets/hymns/HisEyeIsOnTheSparrow-0.jpg'),
       require('../assets/hymns/HisEyeIsOnTheSparrow-1.jpg'),
@@ -39,6 +43,7 @@ const songEntries = [
   {
     id: "5",
     title: "Holy God We Praise Thy Name",
+    category: "hymn",
     images: [
       require('../assets/hymns/HolyGodWePraiseThyName.jpg'),
       // Add more images as needed
@@ -47,6 +52,7 @@ const songEntries = [
   {
     id: "6",
     title: "Holy Holy Holy",
+    category: "hymn",
     images: [
       require('../assets/hymns/HolyHolyHoly-0.jpg'),
       require('../assets/hymns/HolyHolyHoly-1.jpg'),
@@ -56,6 +62,7 @@ const songEntries = [
   {
     id: "7",
     title: "How Great Thou Art",
+    category: "hymn",
     images: [
       require('../assets/hymns/HowGreatThouArt-0.jpg'),
       require('../assets/hymns/HowGreatThouArt-1.jpg'),
@@ -66,6 +73,7 @@ const songEntries = [
   {
     id: "8",
     title: "I'll Praise My Maker While I've Breath",
+    category: "hymn",
     images: [
       require('../assets/hymns/IllPraiseMyMakerWhileIveBreath.jpg'),
       // Add more images as needed
@@ -74,6 +82,7 @@ const songEntries = [
   {
     id: "9",
     title: "In The Garden",
+    category: "hymn",
     images: [
       require('../assets/hymns/InTheGarden-0.jpg'),
       require('../assets/hymns/InTheGarden-1.jpg'),
@@ -83,6 +92,7 @@ const songEntries = [
   {
     id: "10",
     title: "It Is Well",
+    category: "hymn",
     images: [
       require('../assets/hymns/ItIsWell-0.jpg'),
       require('../assets/hymns/ItIsWell-1.jpg'),
@@ -92,6 +102,7 @@ const songEntries = [
   {
     id: "11",
     title: "Jesus Loves Me",
+    category: "hymn",
     images: [
       require('../assets/hymns/JesusLovesMe-0.jpg'),
       require('../assets/hymns/JesusLovesMe-1.jpg'),
@@ -101,6 +112,7 @@ const songEntries = [
   {
     id: "12",
     title: "Just As I Am",
+    category: "hymn",
     images: [
       require('../assets/hymns/JustAsIAm.jpg'),
       // Add more images as needed
@@ -109,6 +121,7 @@ const songEntries = [
   {
     id: "13",
     title: "Men And Children Everywhere",
+    category: "hymn",
     images: [
       require('../assets/hymns/MenAndChildrenEverywhere.jpg'),
       // Add more images as needed
@@ -117,6 +130,7 @@ const songEntries = [
   {
     id: "14",
     title: "Nearer My God To Thee",
+    category: "hymn",
     images: [
       require('../assets/hymns/NearerMyGodToThee-0.jpg'),
       require('../assets/hymns/NearerMyGodToThee-1.jpg'),
@@ -126,6 +140,7 @@ const songEntries = [
   {
     id: "15",
     title: "Redeemed",
+    category: "hymn",
     images: [
       require('../assets/hymns/Redeemed-0.jpg'),
       require('../assets/hymns/Redeemed-1.jpg'),
@@ -134,7 +149,8 @@ const songEntries = [
   },
   {
     id: "16",
-    title: "Seek Ye First",
+    title: "Seek Ye First (1)",
+    category: "hymn",
     images: [
       require('../assets/hymns/SeekYeFirst-0.jpg'),
       require('../assets/hymns/SeekYeFirst-1.jpg'),
@@ -143,7 +159,8 @@ const songEntries = [
   },
   {
     id: "17",
-    title: "Seek Ye First",
+    title: "Seek Ye First (2)",
+    category: "hymn",
     images: [
       require('../assets/hymns/SeekYeFirst1-0.jpg'),
       require('../assets/hymns/SeekYeFirst1-1.jpg'),
@@ -153,6 +170,7 @@ const songEntries = [
   {
     id: "18",
     title: "See The Morning Sun Ascending",
+    category: "hymn",
     images: [
       require('../assets/hymns/SeeTheMorningSunAscending.jpg'),
       // Add more images as needed
@@ -161,6 +179,7 @@ const songEntries = [
   {
     id: "19",
     title: "We Thy People Praise Thee",
+    category: "hymn",
     images: [
       require('../assets/hymns/WeThyPeoplePraiseThee-0.jpg'),
       require('../assets/hymns/WeThyPeoplePraiseThee-1.jpg'),
@@ -170,6 +189,7 @@ const songEntries = [
   {
     id: "20",
     title: "What A Friend We Have In Jesus",
+    category: "hymn",
     images: [
       require('../assets/hymns/WhatAFriendWeHaveInJesus-0.jpg'),
       require('../assets/hymns/WhatAFriendWeHaveInJesus-1.jpg'),
@@ -179,7 +199,8 @@ const songEntries = [
   },
   {
     id: "21",
-    title: "When I Survey Thy Wondrous Cross",
+    title: "When I Survey Thy Wondrous Cross (1)",
+    category: "hymn",
     images: [
       require('../assets/hymns/WhenISurvey-0.jpg'),
       require('../assets/hymns/WhenISurvey-1.jpg'),
@@ -188,7 +209,8 @@ const songEntries = [
   },
   {
     id: "22",
-    title: "When I Survey Thy Wondrous Cross",
+    title: "When I Survey Thy Wondrous Cross (2)",
+    category: "hymn",
     images: [
       require('../assets/hymns/WhenISurveyTheWondrousCross-0.jpg'),
       require('../assets/hymns/WhenISurveyTheWondrousCross-1.jpg'),
@@ -198,13 +220,74 @@ const songEntries = [
   {
     id: "23",
     title: "Without Him",
+    category: "hymn",
     images: [
       require('../assets/hymns/WithoutHim-0.jpg'),
       require('../assets/hymns/WithoutHim-1.jpg'),
       // Add more images as needed
     ],
   },
-  // Add more entries as needed
+  {
+    id: "24",
+    title: "Blue Danube Waltz",
+    category: "popular",
+    images: [
+      require('../assets/hymns/BlueDanubeWaltz-0.jpg'),
+      require('../assets/hymns/BlueDanubeWaltz-1.jpg'),
+      // Add more images as needed
+    ],
+  },
+  {
+    id: "25",
+    title: "I've Got Peace",
+    category: "hymn",
+    images: [
+      require('../assets/hymns/IveGotPeace-0.jpg'),
+      require('../assets/hymns/IveGotPeace-1.jpg'),
+
+    ],
+  },
+  {
+    id: "26",
+    title: "My Favorite Things",
+    category: "popular",
+    images: [
+      require('../assets/hymns/MyFavoriteThings-0.jpg'),
+      require('../assets/hymns/MyFavoriteThings-1.jpg'),
+      require('../assets/hymns/MyFavoriteThings-2.jpg'),
+
+    ],
+  },
+  {
+    id: "27",
+    title: "Pop Goes The Weasel",
+    category: "children",
+    images: [
+      require('../assets/hymns/PopGoesTheWeasel.jpg'),
+
+    ],
+  },
+  {
+    id: "28",
+    title: "You Are My Sunshine",
+    category: "popular",
+    images: [
+      require('../assets/hymns/YouAreMySunshine-0.jpg'),
+      require('../assets/hymns/YouAreMySunshine-1.jpg'),
+
+    ],
+  },
+  {
+    id: "29",
+    title: "Surely The Presence",
+    category: "hymn",
+    images: [
+      require('../assets/hymns/SurelyThePresence-0.jpg'),
+      require('../assets/hymns/SurelyThePresence-1.jpg'),
+
+    ],
+  },
+
 ];
 
 export default songEntries;
